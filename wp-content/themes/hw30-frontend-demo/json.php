2764d01bfea785ed4645fbea40f5e7a9359e14fa
msplint <tk@terrifickid.net>
2022-01-16 11:13:04 -0500

    auto-deploy

--- wp-content/themes/hw30-frontend-demo/json.php
+++ wp-content/themes/hw30-frontend-demo/json.php
@@ -6,3 +6,12 @@ Template Name: JSON layout
 // Page code here..
 
 echo 'test!';
+// WP_Query arguments
+$args = array(
+	'post_type'              => array( 'h_timeline' ),
+	'nopaging'               => true,
+  'posts_per_page'         => -1
+);
+
+// The Query
+$query = new WP_Query( $args );
