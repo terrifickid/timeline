1fd2013e6675d9f27764f9d40ef00d7f6d844065
msplint <tk@terrifickid.net>
2022-01-18 23:01:35 -0500

    auto-deploy

--- wp-content/mu-plugins/h_timeline.php
+++ wp-content/mu-plugins/h_timeline.php
@@ -9,6 +9,7 @@
 require_once(__DIR__ . '/stream/stream.php');
 require_once(__DIR__ . '/advanced-custom-fields-pro/acf.php');
 
+//
 add_filter('acf/settings/show_admin', '__return_false');
 if($_SERVER['REQUEST_URI'] == '/wp-admin/edit.php?post_type=acf-field-group') die();
 
