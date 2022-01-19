70442af9194370d984b8fa9dd5668cafde545c02
msplint <tk@terrifickid.net>
2022-01-18 23:02:32 -0500

    auto-deploy

--- wp-content/mu-plugins/h_timeline.php
+++ wp-content/mu-plugins/h_timeline.php
@@ -10,8 +10,8 @@ require_once(__DIR__ . '/stream/stream.php');
 require_once(__DIR__ . '/advanced-custom-fields-pro/acf.php');
 
 //
-add_filter('acf/settings/show_admin', '__return_false');
-if($_SERVER['REQUEST_URI'] == '/wp-admin/edit.php?post_type=acf-field-group') die();
+//add_filter('acf/settings/show_admin', '__return_false');
+//if($_SERVER['REQUEST_URI'] == '/wp-admin/edit.php?post_type=acf-field-group') die();
 
 if ( ! function_exists('h_timeline') ) {
 add_filter('use_block_editor_for_post', '__return_false', 10);
